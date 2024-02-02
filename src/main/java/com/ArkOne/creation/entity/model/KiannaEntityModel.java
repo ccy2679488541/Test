80d446f64adf163e76e3f1c0073e76255a08610c
hp <2679488541@qq.com>
2024-02-02 13:29:28 +0800

    git测试

--- src/main/java/com/ArkOne/creation/entity/model/KiannaEntityModel.java
+++ src/main/java/com/ArkOne/creation/entity/model/KiannaEntityModel.java
@@ -15,8 +15,8 @@ public class KiannaEntityModel extends EntityModel<KiannaEntity> {
     private final ModelRenderer right_leg;
 
     public KiannaEntityModel() {
-        texHeight =256;
-        texWidth = 256;
+        texHeight =64;
+        texWidth = 64;
 
         head = new ModelRenderer(this);
         head.setPos(0.0F, 24.0F, 0.0F);
@@ -24,23 +24,23 @@ public class KiannaEntityModel extends EntityModel<KiannaEntity> {
 
         left_leg = new ModelRenderer(this);
         left_leg.setPos(0.0F, 24.0F, 0.0F);
-        left_leg.setTexSize(64, 128).addBox(-4.0F, -12.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, false);
+        left_leg.setTexSize(16, 32).addBox(-4.0F, -12.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, false);
 
         left_arm = new ModelRenderer(this);
         left_arm.setPos(0.0F, 24.0F, 0.0F);
-        left_arm.setTexSize(128, 0).addBox(-8.0F, -24.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, false);
+        left_arm.setTexSize(32, 0).addBox(-8.0F, -24.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, false);
 
         body = new ModelRenderer(this);
         body.setPos(0.0F, 24.0F, 0.0F);
-        body.setTexSize(0, 60).addBox(-4.0F, -24.0F, -2.0F, 8.0F, 12.0F, 4.0F, 0.0F, false);
+        body.setTexSize(0, 16).addBox(-4.0F, -24.0F, -2.0F, 8.0F, 12.0F, 4.0F, 0.0F, false);
 
         right_arm = new ModelRenderer(this);
         right_arm.setPos(0.0F, 24.0F, 0.0F);
-        right_arm.setTexSize(0, 128).addBox(4.0F, -24.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, false);
+        right_arm.setTexSize(0, 32).addBox(4.0F, -24.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, false);
 
         right_leg = new ModelRenderer(this);
         right_leg.setPos(0.0F, 24.0F, 0.0F);
-        right_leg.setTexSize(96, 64).addBox(0.0F, -12.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, false);
+        right_leg.setTexSize(24, 16).addBox(0.0F, -12.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, false);
     }
 
 
